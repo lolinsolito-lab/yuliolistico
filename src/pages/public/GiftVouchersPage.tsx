1744f3d3d976b0b8c238cc13f8f4b48b00e37a75
lolinsolito-lab <lordinsolito@gmail.com>
2026-08-26 19:17:04 +0200

    fix: create missing GiftVouchersPage to resolve Vercel build error

---
+++ src/pages/public/GiftVouchersPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GiftCards from '../../components/GiftCards';
+
+const GiftVouchersPage: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-[#faf9f6] pt-24">
+      <GiftCards />
+    </div>
+  );
+};
+
+export default GiftVouchersPage;
